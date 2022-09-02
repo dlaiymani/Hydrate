ca36ed3504523f316548ddee463e6086ed14ac3c
David Laiymani <david.laiymani@icloud.com>
2022-09-02 15:29:37 +0200

    Initial Commit

---
+++ Hydrate/HydrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrateApp.swift
+//  Hydrate
+//
+//  Created by David Laiymani on 02/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct HydrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
